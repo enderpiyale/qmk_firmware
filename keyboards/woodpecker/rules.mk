8df3922e1562e9f060a0e647361dac2068772a1c
enderpiyale <enderpiyale@gmail.com>
2024-02-04 23:04:40 +0300

    test

--- keyboards/woodpecker/rules.mk
+++ keyboards/woodpecker/rules.mk
@@ -1,5 +1,8 @@
 # This file intentionally left blank
 OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
 OLED_TRANSPORT = i2c
-OLED_BRIGHTNESS = 128
+
 LTO_ENABLE = yes
